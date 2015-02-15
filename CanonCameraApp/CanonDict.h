c0303237f48ee1498d8e1dae9ea259f6cdaf07d0
manu o <djabolo@gmail.com>
2015-02-15 18:14:57 +0100

    comments

--- CanonCameraApp/CanonDict.h
+++ CanonCameraApp/CanonDict.h
@@ -6,31 +6,36 @@ class CanonDict
 {
 public:
 
+    //get the singleton
     static CanonDict* getInstance();
     
+    //Destructor
     virtual ~CanonDict(void);
 
+    //transform object event id into string
     static std::string ObjectEventToString(unsigned long objEvent_);
 
+    //transform property event id into string
     static std::string PropertyEventToString(unsigned long propEvent_);
 
+    //transform state event id into string
     static std::string StateEventToString(unsigned long propEvent_);
 
-
-    //static std::string
-
 protected :
+    //object event map
     static std::map<unsigned long, std::string> _objectEventDict;
 
+    //property event map
     static std::map<unsigned long, std::string> _propEventDict;
 
+    //state event map
     static std::map<unsigned long, std::string> _stateEventDict;
 
+    //The unique instance
     static CanonDict* _instance;
 
+    //protected constructor
     CanonDict(void);
     
-
-
 };
 
